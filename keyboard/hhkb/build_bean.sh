4bf317fd5df8706b405909e284313b93ed576a98
Jongbin Jung <jungjongbin86@gmail.com>
2025-11-18 22:13:10 -0800

    Update personal build script

--- keyboard/hhkb/build_bean.sh
+++ keyboard/hhkb/build_bean.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/env bash
 
+# Prerequisite packages
+git submodule update --init
+sudo dnf install avr-gcc avr-libc libusb1-devel automake
+
 # NOTES TO SELF
-# -- Only works with dfu-programmer 0.6.2 (does NOT work with 0.7.x)
-# -- requires avr-gcc and avr-libc
+# -- Used to only work with dfu-programmer 0.6.2 (does NOT work with 0.7.x)
+# -- Now works with latest compiled from source (c204739)
+cd ~/repos
+git clone git@github.com:dfu-programmer/dfu-programmer.git
+cd dfu-programmer
+./bootstrap.sh
+./configure
+make
+sudo make install
+
+cd ~/repos/bean_keyboard/keyboard/hhkb
 make clean
 make KEYMAP=bean
 sudo make KEYMAP=bean dfu
